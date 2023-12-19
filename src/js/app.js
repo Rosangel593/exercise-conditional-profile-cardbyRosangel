9d2aa711c1d90dea4a3ea817b3f7af37fdb9f167
Rosangel593 <148271323+Rosangel593@users.noreply.github.com>
2023-12-19 20:35:47 +0000

    por terminar

--- src/js/app.js
+++ src/js/app.js
@@ -41,11 +41,22 @@ function render(variables = {}) {
     variables.country == null ? "Country" : variables.country
   }</h3>
           <ul class="${variables.socialMediaPosition}">
-            <li><a href="https://twitter.com/4geeksacademy" >
+          <li><a href="https://twitter.com/${
+            variables.twitter ? variables.twitter : "4geeksacademy"
+          }">
             <i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="https://github.com/ ${
+              variables.github ? variables.github : "4geeksacademy"
+            }">
+            <i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/ ${
+              variables.linkedin ? variables.linkedin : "4geeksacademy"
+            }">
+            <i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/ ${
+              variables.instagram ? variables.linkedin : "4geeksacademy"
+            }">
+              <i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
